560e3abb035b0fd51c489140d1b469cfa3ea4564
sumanjalichapala3-cpu <sumanjalichapala3@gmail.com>
2026-07-13 15:13:36 +0530

    Modify database connection settings
    Updated MongoDB connection string to use SRV format and added DNS servers.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-
+const dns = require("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
   try {
     
-    await mongoose.connect('mongodb://localhost:27017/workshop_db');
+    await mongoose.connect('mongodb+srv://sumanjalichapala24:suma@1722007@cluster0.fkc7o9w.mongodb.net/');
     
     console.log('MongoDB connected successfully!');
   } catch (err) {
