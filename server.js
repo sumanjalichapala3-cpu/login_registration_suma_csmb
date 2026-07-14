d5cc5e6d297b8c9a700cff4884919234d57d19ee
sumanjalichapala3-cpu <sumanjalichapala3@gmail.com>
2026-07-14 11:54:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-
-const connectDB = require('./config/db.js');
-const authRoutes = require('./routes/auth.js');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
 const app = express();
+const PORT = 5000;
+
 
 connectDB();
 
-app.use(cors());
 app.use(express.json());
+
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running smoothly on port ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
